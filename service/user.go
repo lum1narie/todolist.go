461bf30d827afeb3dc2006bcaec156eb2b0ae94d
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2022-11-17 07:57:34 +0900

    :zap: implemented error msg in user adding page

--- service/user.go
+++ service/user.go
@@ -2,9 +2,10 @@ package service
 
 import (
 	"crypto/sha256"
-    "net/http"
-	
-    "github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	database "todolist.go/db"
 )
 
@@ -13,47 +14,80 @@ const (
 )
 
 func hash(pw string) []byte {
-    h := sha256.New()
-    h.Write([]byte(salt))
-    h.Write([]byte(pw))
-    return h.Sum(nil)
+	h := sha256.New()
+	h.Write([]byte(salt))
+	h.Write([]byte(pw))
+	return h.Sum(nil)
 }
 
 func RegisterUser(ctx *gin.Context) {
-    // フォームデータの受け取り
-    username := ctx.PostForm("username")
-    password := ctx.PostForm("password")
-    if username == "" || password == "" {
-        Error(http.StatusBadRequest, "Empty parameter")(ctx)
-        return
-    }
-    
-    // DB 接続
-    db, err := database.GetConnection()
-    if err != nil {
-        Error(http.StatusInternalServerError, err.Error())(ctx)
-        return
-    }
-	
-    // DB への保存
-    result, err := db.Exec("INSERT INTO users(name, password) VALUES (?, ?)", username, hash(password))
-    if err != nil {
-        Error(http.StatusInternalServerError, err.Error())(ctx)
-        return
-    }
-	
-    // 保存状態の確認
-    id, _ := result.LastInsertId()
-    var user database.User
-    err = db.Get(&user, "SELECT id, name, password FROM users WHERE id = ?", id)
-    if err != nil {
-        Error(http.StatusInternalServerError, err.Error())(ctx)
-        return
-    }
-    ctx.JSON(http.StatusOK, user)
+	// フォームデータの受け取り
+	username := ctx.PostForm("username")
+	password := ctx.PostForm("password")
+	switch {
+	case username == "":
+		ctx.HTML(http.StatusBadRequest, "new_user_form.html",
+			gin.H{
+				"Title":    "Register user",
+				"Error":    "Usernane is not provided",
+				"Username": username,
+			})
+	case password == "":
+		ctx.HTML(http.StatusBadRequest, "new_user_form.html",
+			gin.H{
+				"Title":    "Register user",
+				"Error":    "Password is not provided",
+				"Password": password,
+			})
+	}
+
+	// DB 接続
+	db, err := database.GetConnection()
+	if err != nil {
+		Error(http.StatusInternalServerError, err.Error())(ctx)
+		return
+	}
+
+	// 重複チェック
+	var duplicate int
+	err = db.Get(&duplicate,
+		"SELECT COUNT(*) FROM users WHERE name=?", username)
+	if err != nil {
+		Error(http.StatusInternalServerError, err.Error())(ctx)
+		return
+	}
+	if duplicate > 0 {
+		ctx.HTML(http.StatusBadRequest, "new_user_form.html",
+			gin.H{
+				"Title": "Register user",
+				"Error": fmt.Sprintf(
+					"Username '%s' is already taken", username),
+				"Username": username,
+				"Password": password,
+			})
+		return
+	}
+
+	// DB への保存
+	result, err := db.Exec(
+		"INSERT INTO users(name, password) VALUES (?, ?)",
+		username, hash(password))
+	if err != nil {
+		Error(http.StatusInternalServerError, err.Error())(ctx)
+		return
+	}
+
+	// 保存状態の確認
+	id, _ := result.LastInsertId()
+	var user database.User
+	err = db.Get(&user, "SELECT id, name, password FROM users WHERE id = ?", id)
+	if err != nil {
+		Error(http.StatusInternalServerError, err.Error())(ctx)
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
 
 func NewUserForm(ctx *gin.Context) {
-    ctx.HTML(http.StatusOK, "new_user_form.html", gin.H{"Title": "Register user"})
+	ctx.HTML(http.StatusOK, "new_user_form.html", gin.H{"Title": "Register user"})
 }
-
